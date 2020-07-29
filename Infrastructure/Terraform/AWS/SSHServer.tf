ab9f9d5550aba0facc85fcb61616824162e97cf7
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-30 06:11:07 +0900

    持ってきていたamiはarmのものだったのでx86の方に変更。

--- Infrastructure/Terraform/AWS/SSHServer.tf
+++ Infrastructure/Terraform/AWS/SSHServer.tf
@@ -14,8 +14,7 @@ module "SSHServer" {
 
   name = "SlotSNS_SSHServer"
   instance_count = 1
-  //ami = "ami-066b76d09a3d3ff4e"     // Amazon Linux
-  ami = "ami-07f31579a94d28584"     // Amazon Linux 2
+  ami = "ami-0cc75a8978fbbc969"     // Amazon Linux2
   instance_type = "t2.micro"
   key_name = var.key_name
   vpc_security_group_ids = [module.SSH_SecurityGroup.this_security_group_id]
