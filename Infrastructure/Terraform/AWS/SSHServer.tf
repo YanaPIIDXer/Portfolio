9a77025f1421c3c0d229aa34a94eb8291fcdeea8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-29 21:21:03 +0900

    Amazon Linux → Amazon Linux2に変更。

--- Infrastructure/Terraform/AWS/SSHServer.tf
+++ Infrastructure/Terraform/AWS/SSHServer.tf
@@ -3,7 +3,7 @@ module "SSHServer" {
 
   name = "SlotSNS_SSHServer"
   instance_count = 1
-  ami = "ami-066b76d09a3d3ff4e"
+  ami = "ami-07f31579a94d28584"
   instance_type = "t2.micro"
   key_name = var.key_name
   vpc_security_group_ids = [module.SecurityGroup.this_security_group_id]
