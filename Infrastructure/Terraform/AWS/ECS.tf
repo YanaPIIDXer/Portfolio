8af0f1db2a2e36e9c41accdba4bde261d154a3e8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-30 08:29:15 +0900

    セキュリティグループを定義。

--- Infrastructure/Terraform/AWS/ECS.tf
+++ Infrastructure/Terraform/AWS/ECS.tf
@@ -1,3 +1,14 @@
+module "InnerAPI_SecurityGroup" {
+    source = "terraform-aws-modules/security-group/aws"
+
+    name = "InnerAPI_SecurityGroup"
+    description = "InnerAPI Security Group"
+    vpc_id = module.VPC.vpc_id
+
+    ingress_cidr_blocks = ["0.0.0.0/0"]
+    ingress_rules = ["http-80-tcp", "ssh-tcp"]
+}
+
 module "TaskDef_InnerAPI" {
     source = "./ECS/TaskDefinition"
 
@@ -15,6 +26,6 @@ module "Cluster_InnerAPI" {
     name = "InnerAPI"
     family = "InnerAPI"
     task = module.TaskDef_InnerAPI.arn
-    security_groups = []
+    security_groups = [module.InnerAPI_SecurityGroup.this_security_group_id]
     subnets = [module.VPC.public_subnets[0]]
 }
