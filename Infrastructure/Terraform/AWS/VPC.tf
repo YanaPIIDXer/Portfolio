1605421ef2f7f636a8e78b36fe9598cff5d98956
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-29 20:48:01 +0900

    cidrが衝突していたので修正。 基礎知識だが参考：https://www.youtube.com/watch?v=mHi2yFWLz9M

--- Infrastructure/Terraform/AWS/VPC.tf
+++ Infrastructure/Terraform/AWS/VPC.tf
@@ -2,11 +2,11 @@ module "VPC" {
   source = "terraform-aws-modules/vpc/aws"
   
   name = "SlotSNS_VPC"
-  cidr = "10.0.0.0/16"
+  cidr = "10.0.0.0/21"
 
   azs = ["ap-northeast-1a", "ap-northeast-1c"]
   public_subnets = ["10.0.1.0/24"]
-  private_subnets = ["10.0.2.0/24", "10.0.2.1/24"]
+  private_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = true
