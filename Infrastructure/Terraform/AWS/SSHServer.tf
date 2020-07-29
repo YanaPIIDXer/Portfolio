e9041da4c648d2d98d2961422ddee7063c31f7a7
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-29 22:43:47 +0900

    逆だったので修正。

--- Infrastructure/Terraform/AWS/SSHServer.tf
+++ Infrastructure/Terraform/AWS/SSHServer.tf
@@ -4,10 +4,10 @@ module "SSHServer" {
   name = "SlotSNS_SSHServer"
   instance_count = 1
   // ↓Amazon Linux
-  ami = "ami-07f31579a94d28584"
+  ami = "ami-066b76d09a3d3ff4e"
   // ↓Amazon Linux2
   //  何か知らんけどRDSが依存してる上にこれに変えるとRDSの再構築に失敗する。
-  //ami = "ami-066b76d09a3d3ff4e"
+  //ami = "ami-07f31579a94d28584"
   instance_type = "t2.micro"
   key_name = var.key_name
   vpc_security_group_ids = [module.SecurityGroup.this_security_group_id]
