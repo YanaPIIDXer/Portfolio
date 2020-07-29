659cc75564800d0304453a5e38b4fad4a832d184
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-30 07:36:27 +0900

    何故か消えていたので復旧。

--- Infrastructure/Terraform/AWS/ECS/TaskDefinition/main.tf
+++ Infrastructure/Terraform/AWS/ECS/TaskDefinition/main.tf
@@ -1,3 +1,23 @@
 resource "aws_ecs_task_definition" "default" {
+  family = var.family
 
+  requires_compatibilities = ["EC2"]
+  cpu = var.cpu
+  memory = var.memory
+  network_mode = "awsvpc"
+
+  container_definitions = <<EOL
+    [
+        {
+            "name": "${var.container_name}",
+            "image": "${var.image}",
+            "portMappings": [
+                {
+                    "containerPort": ${var.container_port},
+                    "hostPort": ${var.host_port}
+                }
+            ]
+        }
+    ]
+    EOL
 }
