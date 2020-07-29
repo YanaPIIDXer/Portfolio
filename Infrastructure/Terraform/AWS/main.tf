754c9f9f9f01dd18c49ab73d9cee4eed5f27fa27
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-29 13:36:54 +0900

    VPCの構成を再度定義。

--- Infrastructure/Terraform/AWS/main.tf
+++ Infrastructure/Terraform/AWS/main.tf
@@ -3,6 +3,21 @@ provider "aws" {
   region  = "ap-northeast-1"
 }
 
-module "SlotSNS_VPC" {
-  source = "./VPC"
+module "VPC" {
+  source = "./terraform-aws-vpc-2.44.0"
+  
+  name = "SlotSNS_VPC"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-1a"]
+  private_subnets = ["10.0.0.1/24"]
+  public_subnets  = ["10.0.0.2/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
