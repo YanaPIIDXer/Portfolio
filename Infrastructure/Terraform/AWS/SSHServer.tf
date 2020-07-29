33229de192fd829278a049c86ee096bc8ba3c441
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-29 22:39:57 +0900

    何故かRDSがSSHサーバに依存している上にこのamiにするとRDSの再構築に失敗するので元に戻した。

--- Infrastructure/Terraform/AWS/SSHServer.tf
+++ Infrastructure/Terraform/AWS/SSHServer.tf
@@ -3,7 +3,11 @@ module "SSHServer" {
 
   name = "SlotSNS_SSHServer"
   instance_count = 1
+  // ↓Amazon Linux
   ami = "ami-07f31579a94d28584"
+  // ↓Amazon Linux2
+  //  何か知らんけどRDSが依存してる上にこれに変えるとRDSの再構築に失敗する。
+  //ami = "ami-066b76d09a3d3ff4e"
   instance_type = "t2.micro"
   key_name = var.key_name
   vpc_security_group_ids = [module.SecurityGroup.this_security_group_id]
