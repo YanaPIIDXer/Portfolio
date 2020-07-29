1771e9497c8ea8aabbf6deffc9c02d165c18d0e8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-29 13:08:07 +0900

    パブリックサブネットの設定の定義を追加。

--- Infrastructure/Terraform/AWS/VPC/main.tf
+++ Infrastructure/Terraform/AWS/VPC/main.tf
@@ -1,3 +1,7 @@
+variable "VPC_AvailabilityZone" {
+    default = "ap-northeast-1a"
+}
+
 resource "aws_vpc" "SlotSNS_VPC" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
@@ -10,4 +14,10 @@ resource "aws_vpc" "SlotSNS_VPC" {
 
 resource "aws_internet_gateway" "SlotSNS_InternetGateway" {
     vpc_id = aws_vpc.SlotSNS_VPC.id
+}
+
+resource "aws_subnet" "SlotSNS_Subnet_Public" {
+    vpc_id = aws_vpc.SlotSNS_VPC.id
+    cidr_block = "10.0.0.2/24"
+    availability_zone = var.VPC_AvailabilityZone
 }
