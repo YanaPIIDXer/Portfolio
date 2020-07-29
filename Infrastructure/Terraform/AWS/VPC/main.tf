c2c7fd4ab6ab0a2ff6350184a9bf14db63de07eb
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-29 13:10:34 +0900

    ルートテーブルの設定を定義。

--- Infrastructure/Terraform/AWS/VPC/main.tf
+++ Infrastructure/Terraform/AWS/VPC/main.tf
@@ -21,3 +21,16 @@ resource "aws_subnet" "SlotSNS_Subnet_Public" {
     cidr_block = "10.0.0.2/24"
     availability_zone = var.VPC_AvailabilityZone
 }
+
+resource "aws_route_table" "SlotSNS_RouteTable_Public" {
+    vpc_id = aws_vpc.SlotSNS_VPC.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.SlotSNS_InternetGateway.id
+    }
+}
+
+resource "aws_route_table_association" "SlotSNS_RouteTable_Association_Public" {
+    subnet_id = aws_subnet.SlotSNS_Subnet_Public.id
+    route_table_id = aws_route_table.SlotSNS_RouteTable_Public.id
+}
