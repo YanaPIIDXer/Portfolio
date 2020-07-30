517d7ddd276b472d5770ae0e224bcbf5488c03d4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-30 11:48:52 +0900

    クラスタに対してECSインスタンスがぶら下がるようにした。

--- Infrastructure/Terraform/AWS/ECS.tf
+++ Infrastructure/Terraform/AWS/ECS.tf
@@ -6,7 +6,21 @@ module "InnerAPI_SecurityGroup" {
     vpc_id = module.VPC.vpc_id
 
     ingress_cidr_blocks = ["0.0.0.0/0"]
-    ingress_rules = ["http-80-tcp", "ssh-tcp"]
+    ingress_rules = ["http-80-tcp"]
+}
+
+module "ECSAgent_SecurityGroup" {
+    source = "terraform-aws-modules/security-group/aws"
+
+    name = "ECSAgent_SecurityGroup"
+    description = "ECS Agent Security Group"
+    vpc_id = module.VPC.vpc_id
+
+    ingress_cidr_blocks = ["0.0.0.0/0"]
+    ingress_rules = ["ssh-tcp"]
+
+    egress_cidr_blocks = ["0.0.0.0/0"]
+    egress_rules = ["all-all"]
 }
 
 module "TaskDef_InnerAPI" {
@@ -68,8 +82,10 @@ module "InnerAPI_Server" {
     instance_count = 1
     ami = "ami-0763fff45988661c8"     // ECSエージェント入りのami
     instance_type = "t2.micro"
-    key_name = var.key_name
-    vpc_security_group_ids = [module.InnerAPI_SecurityGroup.this_security_group_id]
+    vpc_security_group_ids = [
+        module.InnerAPI_SecurityGroup.this_security_group_id,
+        module.ECSAgent_SecurityGroup.this_security_group_id
+    ]
     subnet_id = module.VPC.public_subnets[0]
     iam_instance_profile = aws_iam_instance_profile.ECS_Role_Profile.name
     user_data = <<USERDATA
