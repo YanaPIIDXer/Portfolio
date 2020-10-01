8d3bd33ec07be91a96841976a2dc7a552710cb34
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-01 19:26:49 +0900

    VPCを定義。

--- Infrastructure/Terraform/AWS/main.tf
+++ Infrastructure/Terraform/AWS/main.tf
@@ -2,3 +2,26 @@ provider "aws" {
   version = "~> 2.0"
   region  = "ap-northeast-1"
 }
+
+// VPC
+module "vpc" {
+  source     = "./VPC"
+  name       = "SlotSNS"
+  cidr_block = "10.1.0.0/16"
+  public_subnets = [
+    {
+      cidr_block        = "10.1.0.0/24"
+      availability_zone = "ap-northeast-1a"
+    }
+  ]
+  private_subnets = [
+    {
+      cidr_block        = "10.1.1.0/24"
+      availability_zone = "ap-northeast-1a"
+    },
+    {
+      cidr_block        = "10.1.2.0/24"
+      availability_zone = "ap-northeast-1c"
+    }
+  ]
+}
